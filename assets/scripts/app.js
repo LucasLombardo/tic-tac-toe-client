1e58b37108a36313e199e453e9684cc79d3b1ab0
LucasLombardo <lucasalombardo@gmail.com>
2019-02-18 19:28:55 -0500

    Finish linking page transitions
    Finishes refactoring to handle simulated page changes.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,27 +11,19 @@ $(() => {
     $(`#sign-out-form`).on(`submit`, authEvents.onSignOut)
     $(`.gameboard--marker`).on(`click`, gameEvents.onSelectSpace)
     $(`#reset`).on(`click`, gameEvents.onReset)
-    $(`#get-game-history`).on(`click`, gameEvents.onGetGameHistory)
 
     $(`.sidenav`).sidenav()
 
     // hide pages
     $(`#auth-page`).hide()
     $(`#history-page`).hide()
-    // temporary page change logic
-    let page = `#game-page`
-    $(`.nm-logo`).on(`click`, () => {
-        if (page === `#game-page`) {
-            page = `#auth-page`
-            $(`#game-page`).fadeOut(`fast`, () => {
-                $(`#auth-page`).fadeIn(`slow`)
-            })
-        } else {
-            page = `#game-page`
-            $(`#auth-page`).fadeOut(`fast`, () => {
-                $(`#game-page`).fadeIn(`slow`)
-            })
-        }
+
+    // to auth
+    $(`#open-auth`).on(`click`, () => {
+        $(`#game-page`).fadeOut(`fast`, () => {
+            $(`#auth-page`).fadeIn(`slow`)
+            $(`.sidenav`).sidenav(`close`)
+        })
     })
 
     // back from auth
@@ -40,4 +32,34 @@ $(() => {
             $(`#game-page`).fadeIn(`slow`)
         })
     })
+
+    // to history
+    $(`#open-history`).on(`click`, () => {
+        if (gameEvents.onGetGameHistory()) {
+            // if user signed in, go to history page
+            $(`#auth-page`).fadeOut(`fast`)
+            $(`#game-page`).fadeOut(`fast`, () => {
+                $(`#history-page`).fadeIn(`slow`)
+                $(`.sidenav`).sidenav(`close`)
+            })
+        } else {
+            console.log(`hi`)
+            // move user to auth page
+            // to auth
+            $(`#game-page`).fadeOut(`fast`, () => {
+                $(`#auth-page`).fadeIn(`slow`)
+                $(`.sidenav`).sidenav(`close`)
+            })
+        }
+    })
+
+    // back from history
+    $(`#back-from-history`).on(`click`, () => {
+        $(`#history-page`).fadeOut(`fast`, () => {
+            $(`#game-page`).fadeIn(`slow`)
+        })
+    })
+
+    // sign out button on nav
+    $(`#sign-out-link`).on(`click`, authEvents.onSignOut)
 })
