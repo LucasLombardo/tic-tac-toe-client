d474b53693181bc113002446d3f9e64513e777d8
LucasLombardo <lucasalombardo@gmail.com>
2019-02-20 21:40:41 -0500

    Add comments and refactor auth files
    Add comments and refactor both events.js and ui.js for readability.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -6,12 +6,14 @@ const gameUi = require(`../game/ui`)
 
 const onSignUp = event => {
     event.preventDefault()
+    // get form data and create POST object for API
     const form = event.target
     const formData = getFormFields(form)
 
-    // override pw verification behavior of api
+    // override pw verification behavior of api, no one cares that much about these accts
     formData.credentials.password_confirmation = formData.credentials.password
 
+    // call sign up AJAX call, then pass result of promise data to ui functions
     api.signUp(formData)
         .then(ui.signUpSuccess)
         .catch(ui.signUpFailure)
@@ -19,12 +21,15 @@ const onSignUp = event => {
 
 const onSignIn = event => {
     event.preventDefault()
+    // get form data and create POST object for API
     const form = event.target
     const formData = getFormFields(form)
 
+    // call sign in AJAX call, then pass result of promise data to ui functions
     api.signIn(formData)
-        .then((res) => {
-            ui.signInSuccess(res)
+        .then((responseData) => {
+            ui.signInSuccess(responseData)
+            // reset the gameboard
             game.reset()
         })
         .catch(ui.signInFailure)
@@ -32,9 +37,12 @@ const onSignIn = event => {
 
 const onChangePassword = event => {
     event.preventDefault()
+
+    // get form data and create PATCH object for API
     const form = event.target
     const formData = getFormFields(form)
 
+    // call PATCH request and pass payload to ui functions
     api.changePassword(formData)
         .then(ui.changePasswordSuccess)
         .catch(ui.changePasswordFailure)
@@ -42,14 +50,18 @@ const onChangePassword = event => {
 
 const onSignOut = event => {
     event.preventDefault()
+
+    // call signout AJAX request, uses user token in store, no data body
     api.signOut()
         .then(ui.signOutSuccess)
         .catch(ui.signOutFailure)
 
+    // clear game history
     gameUi.clearGameHistory()
 }
 
 const onLoad = () => {
+    // initialize ui once page is loaded
     ui.init()
 }
 
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,66 +4,80 @@ const messages = require(`../messages`)
 const shuffleArr = require(`../../../lib/shuffleArr`)
 
 const signUpSuccess = () => {
+    // select random signUpSuccess message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.signUpSuccess)[0])
     $(`form`).trigger(`reset`)
 }
 
-const signUpFailure = e => {
+const signUpFailure = () => {
+    // select random signUpFailure message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.signUpFailure)[0])
     $(`form`).trigger(`reset`)
 }
 
 const signInSuccess = responseData => {
+    // select random signInSuccess message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.signInSuccess)[0])
     $(`form`).trigger(`reset`)
+    // write user data to the store
     store.user = responseData.user
+    // toggle which forms to display
     $(`#sign-in-form`).hide()
     $(`#sign-up-form`).hide()
     $(`#change-password-form`).show()
     $(`#sign-out-form`).show()
+    // switch sidenav button text and show signout button
     $(`#open-auth`).text(`Change Password`)
     $(`#sign-out-link`).show()
+    // add username to sidenav display and add user rank
     $(`#username`).text(`${store.user.email}`)
     $(`#user-rank`).text(`Pledge`)
 }
 
-const signInFailure = e => {
+const signInFailure = () => {
+    // select random signInFailure message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.signInFailure)[0])
     $(`form`).trigger(`reset`)
 }
 
-const changePasswordSuccess = e => {
+const changePasswordSuccess = () => {
+    // select random changePasswordSuccess message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.changePwSuccess)[0])
     $(`form`).trigger(`reset`)
 }
 
-const changePasswordFailure = e => {
+const changePasswordFailure = () => {
+    // select random changePasswordFailure message to display and clear forms
     $(`#auth-message`).text(shuffleArr(messages.changePwFailure)[0])
     $(`form`).trigger(`reset`)
 }
 
-const signOutSuccess = e => {
+const signOutSuccess = () => {
     // display message and reset forms
     $(`#auth-message`).text(shuffleArr(messages.signOutSuccess)[0])
     $(`form`).trigger(`reset`)
-    // clear user token
+    // clear user data
     store.user.token = ``
-    // hide unrelevant forms / show relevant ones
+    // toggle which forms to display
     $(`#change-password-form`).hide()
     $(`#sign-out-form`).hide()
     $(`#sign-in-form`).show()
     $(`#sign-up-form`).show()
+    // switch sidenav button text and hide signout button
     $(`#open-auth`).text(`Sign In / Sign Up`)
     $(`#sign-out-link`).hide()
+    // clear username in sidenav display and change rank to not logged in
     $(`#username`).text(``)
     $(`#user-rank`).text(`Not Logged In`)
 }
 
-const signOutFailure = e => {
+const signOutFailure = () => {
+    // display random signOutFailure message
     $(`#auth-message`).text(shuffleArr(messages.signOutFailure)[0])
 }
 
 const init = () => {
+    // hide irrellevant forms
     $(`#change-password-form`).hide()
     $(`#sign-out-form`).hide()
     $(`#sign-out-link`).hide()
